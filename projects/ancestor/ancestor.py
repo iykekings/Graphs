03503dce97b12b2fa1c87cc29e305a47e54b7f6b
Ikechukwu <iykekings36@gmail.com>
2019-11-05 16:11:34 +0100

    a little bit refactoring

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,15 +1,14 @@
 
 def earliest_ancestor(ancestors, starting_node=None):
     # format ancestors to adjacency list
-    #
     graph = {}
-    for val in ancestors:
-      if val[0] in graph:
-        graph[val[0]].append(val[1])
+    for enum in ancestors:
+      key, value = enum
+      if key in graph:
+        graph[key].append(value)
       else:
-        graph[val[0]] = [val[1]]
+        graph[key] = [value]
     print(graph)
-    pass
 
 
 test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
