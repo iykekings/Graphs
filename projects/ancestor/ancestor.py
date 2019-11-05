33927d516f5cbe5a309dbd783bd794c78d7e85f2
Ikechukwu <iykekings36@gmail.com>
2019-11-05 16:05:27 +0100

    format ancestors to adjacency list

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,16 @@
 
-def earliest_ancestor(ancestors, starting_node):
+def earliest_ancestor(ancestors, starting_node=None):
+    # format ancestors to adjacency list
+    #
+    graph = {}
+    for val in ancestors:
+      if val[0] in graph:
+        graph[val[0]].append(val[1])
+      else:
+        graph[val[0]] = [val[1]]
+    print(graph)
     pass
+
+
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+earliest_ancestor(test_ancestors)
